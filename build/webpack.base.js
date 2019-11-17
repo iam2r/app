1c1dfb639404911e64efb59a9f8aa9d768d08a85
zr <bestwishtous@gmail.com>
2019-11-17 17:20:08 +0800

    test actions  多个项目

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -29,7 +29,7 @@ process.env.BUILD_MODE = (() => {
         }
     }
 })();
-process.env.APP_NAME = utils.getAppName('blog');
+process.env.APP_NAME = utils.getAppName('_test');
 
 const createEntryAndHtml = pageList => pageList.reduce((pre, page) => {
     let html = new HtmlWebpackPlugin({
