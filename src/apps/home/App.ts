d9821cc687f6559a9019141c8496ea5f64e4e447
zhangrui <zhangrui@m-apps.com.cn>
2019-12-05 16:18:32 +0800

    home 开发

--- src/apps/home/App.ts
+++ src/apps/home/App.ts
@@ -9,6 +9,17 @@ import ScrollView from "app.root/components/scrollview/ScrollView.vue"
 })
 export default class App extends Vue {
     protected appList: string[] = state.appList;
-    protected isTop: boolean = true;
+    protected topScrolled: boolean = false;
+    protected mainTitle: string = 'iam2r';
+
+    protected created() {
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset > 165 && !this.topScrolled) {
+                this.topScrolled = true
+            } else if (window.pageYOffset <= 165 && this.topScrolled) {
+                this.topScrolled = false
+            }
+        })
+    }
 
 }
--- src/apps/home/App.vue
+++ src/apps/home/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <ScrollView id="app">
-    <div id="mobile-bar" :class="[isTop?'top':'']">
+  <div id="app">
+    <div id="mobile-bar" :class="[!topScrolled?'top':'']">
       <i class="menu-button"></i>
       <i class="logo"></i>
     </div>
     <div id="header">
       <div class="logo">
-         <img class="header-logo" :src="require('./assets/logo.ico')" alt="home logo" />
-         <span>iam2r</span>
+        <img class="header-logo" :src="require('./assets/logo.ico')" alt="home logo" />
+        <span>{{mainTitle}}</span>
       </div>
       <ul class="nav"></ul>
     </div>
@@ -17,12 +17,19 @@
           <img class="hero-logo" :src="require('./assets/logo.ico')" alt="home logo" />
         </div>
         <div class="right">
-          <h2>iam2r</h2>
-          <h1>欢迎<br> 进入我的主页！</h1>
+          <h2>{{mainTitle}}</h2>
+          <h1>
+            欢迎
+            <br />进入我的主页！
+          </h1>
         </div>
       </div>
+      <div class="test" style="height:100vh"></div>
     </div>
-  </ScrollView>
+    <div id="footer">
+      <p>Copyright © 2014-2020 ZhangRui</p>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import App from "./App";
