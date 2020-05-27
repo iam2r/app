0889ad3d2d90cbeba7c97f69f10af1eb0571637b
zhangrui <zhangrui@m-apps.com.cn>
2020-05-27 10:46:24 +0800

    屏幕常亮demo

--- src/apps/test-react/pages/Home/index.tsx
+++ src/apps/test-react/pages/Home/index.tsx
@@ -1,14 +1,31 @@
 import styles from "./styles.module.scss";
 import Count from "app.root/components/Count";
 import { Store } from "app.root/hooks";
-
+import { useState } from "react";
+import NoSleep from "app.root/nosleep";
+const noSleep1 = new NoSleep();
 const Home = (props: any) => {
   const { state } = Store();
+  const [noSleep, setNoSleep] = useState(false);
+  let noSleepTimer: any = -1;
+  const enable = () => {
+    setNoSleep(true);
+    noSleep1.enable();
+  };
+
+  const disable = () => {
+    setNoSleep(false);
+    noSleep1.disable();
+  };
 
   return (
     <div className={styles.welcome}>
       <Count> </Count>
-      {JSON.stringify(state)}
+      <div>
+        <button onClick={enable}>开启</button>
+        <button onClick={disable}>关闭</button>
+        <div>{noSleep ? "已开启常亮模式" : "会休眠"}</div>
+      </div>
     </div>
   );
 };
