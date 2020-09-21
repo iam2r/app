9dce99c58ef11e077d5aaff3c2f9e206e0f36608
rico <iamricogo@gmail.com>
2020-09-21 15:53:49 +0800

    eslint-prettier脚本更新

--- install-eslint-prettier.bat
+++ install-eslint-prettier.bat
@@ -1,1 +1,1 @@
-yarn add eslint prettier eslint-plugin-prettier eslint-plugin-vue  @vue/eslint-config-prettier @vue/eslint-config-typescript  lint-staged  yorkie -D
+yarn add yorkie prettier  lint-staged eslint eslint-plugin-prettier eslint-plugin-vue  @vue/eslint-config-prettier @vue/eslint-config-typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin typescript -D
